f48be792b72249b9c87edf7b53d90467d6adbd32
pitust <piotr@stelmaszek.com>
2021-10-23 21:02:22 +0100

    scheduler: create initial scheduling efforts

---
+++ src/xtrm/user/sched.d
@@ -0,0 +1,47 @@
+module xtrm.user.sched;
+
+import xtrm.memory;
+import xtrm.cpu.cr3;
+import xtrm.interrupt.regs;
+import xtrm.obj.obj;
+import xtrm.obj.vm;
+import xtrm.obj.thread;
+
+private struct ThreadEntry {
+    Thread* thr;
+    ThreadEntry* next;
+    alias thr this;
+}
+private __gshared ThreadEntry* _cur;
+
+Thread* current() { return _cur.thr; }
+
+void init_sched() {
+    _cur = alloc!ThreadEntry;
+    _cur.thr = alloc!Thread;
+    _cur.next = _cur;
+    _cur.vm = alloc!VM;
+    _cur.handles = alloc!(Obj*[512])();
+}
+
+void create_thread(Thread* t) {
+    t.rc++;
+    ThreadEntry* te = alloc!ThreadEntry;
+    te.next = _cur.next;
+    _cur.next = te;
+    te.thr = t;
+}
+
+void sched_yield() {
+    _cur = _cur.next;
+}
+
+void sched_save_preirq(Regs* r) {
+    _cur.regs = *r;
+    copy_from_cr3(_cur.vm.lowhalf);
+}
+
+void sched_restore_postirq(Regs* r) {
+    *r = _cur.regs;
+    copy_to_cr3(_cur.vm.lowhalf);
+}
