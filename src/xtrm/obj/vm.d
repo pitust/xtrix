8ea47c13dadd2f4285e7a7fc513184fe5637fc54
pitust <piotr@stelmaszek.com>
2021-10-24 12:07:48 +0100

    vm: mapping

--- src/xtrm/obj/vm.d
+++ src/xtrm/obj/vm.d
@@ -1,10 +1,39 @@
 module xtrm.obj.vm;
 
+import xtrm.io;
+import xtrm.util;
+import xtrm.memory;
 import xtrm.obj.obj;
-// import xtrm.user.mem;
 
 // struct VMEntry { ulong addr; Memory* mem; }
 struct VM {
     Obj obj = Obj(ObjType.vm); alias obj this;
     ulong[256]* lowhalf;
+
+    private ulong* get_ptr_ptr(ulong va) {
+        assert(lowhalf, "No lowhalf!");
+        ulong[] pte = *lowhalf;
+
+        ulong va_val = va & 0x000f_ffff_ffff_f000;
+        ulong[3] va_values = [
+            ((((va_val >> 12) >> 9) >> 9) >> 9) & 0x1ff,
+            (((va_val >> 12) >> 9) >> 9) & 0x1ff,
+            ((va_val >> 12) >> 9) & 0x1ff,
+        ];
+        foreach (key; va_values) {
+            ulong ptk = pte[key];
+            if (!((ptk) & 1)) {
+                ulong new_page_table = (cast(ulong)alloc!(ubyte[4096])()) - 0xffff800000000000;
+                ptk = pte[key] = 0x07 | new_page_table;
+            }
+            
+            pte = *cast(ulong[512]*)(0xffff800000000000 + ptk & ~0xfff);
+        }
+
+        return &pte[(va_val >> 12) & 0x1ff];
+    }
+
+    void map(ulong va, ulong phy) {
+        *get_ptr_ptr(va) = phys(phy) | 7;
+    }
 }
