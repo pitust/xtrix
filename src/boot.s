cce16579930d873c4b689f7220c0fc8e04cc23d7
pitust <piotr@stelmaszek.com>
2021-10-25 00:19:46 +0100

    Add gplv3 headers to all files

--- src/boot.s
+++ src/boot.s
@@ -1,11 +1,20 @@
-; struct stivale_header {
-;     uint64_t stack;
-;     uint16_t flags;
-;     uint16_t framebuffer_width;
-;     uint16_t framebuffer_height;
-;     uint16_t framebuffer_bpp;
-;     uint64_t entry_point;
-; };
+; kernel assembly entrypoint and stivale1 structure
+; Copyright (C) 2021 pitust <piotr@stelmaszek.com>
+
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 section .text
 global _start
 extern kmain
