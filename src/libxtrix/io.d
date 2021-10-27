6840902979bf8cd76c24ba288541dac0e9aace6e
pitust <piotr@stelmaszek.com>
2021-10-27 14:41:45 +0100

    libxtrix: io: make it do a whole bunch o' types

--- src/libxtrix/io.d
+++ src/libxtrix/io.d
@@ -1,6 +1,7 @@
 module libxtrix.io;
 
 import libxtrix.syscall;
+import libxtrix.intops;
 
 private __gshared char[4096] printf_buffer;
 private __gshared ulong printf_buf_offset = 0;
@@ -8,13 +9,24 @@ private __gshared ulong printf_buf_offset = 0;
 private void putch(char c) {
 	if (c == '\n') {
 		KeLog(printf_buffer.ptr, printf_buf_offset);
+		printf_buf_offset = 0;
 	} else {
 		printf_buffer[printf_buf_offset++] = c;
 	}
 }
 
 void _pvalue(T)(T value) {
-	assert(false, "todo print of type " ~ T.stringof);
+	static if (is(T == ulong)) {
+		_pvalue("<ulong>");	
+	} else static if (is(T == int)) {
+		sprinti(value, 10, 0, "", "", &putch, "", "", "", "", "");
+	} else static if (is(T == string)) {
+		foreach (chr; value) putch(chr);
+	} else static if (is(T : immutable(char)*) || is(T : char*)) {
+		while (*value) putch(*value++);
+	} else {
+		static assert(false, "todo print of type " ~ T.stringof);
+	}
 }
 
 void printf(Args...)(string fmt, Args argz) {
