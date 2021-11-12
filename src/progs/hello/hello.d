6912fb69bd6cfef9739017537794d7a3b3e5374c
pitust <piotr@stelmaszek.com>
2021-11-12 15:19:07 +0000

    progs/hello: clean up the assertions here

--- src/progs/hello/hello.d
+++ src/progs/hello/hello.d
@@ -21,27 +21,26 @@ import libxtrix.syscall;
 import libxtrix.libc.malloc;
 
 /// _start is the OS-invoked entrypoint for xtrix user programs
-extern (C) void _start(ulong phy_stivale2_structure) {
+extern (C) void _start() {
 	XHandle chan = KeCreateChannel();
 	if (chan.isError) assert(false, "KeCreateChannel failed!");
 	{
 		string msg = "hello, channel world!";
 		XHandle data_out = KeAllocateMemRefObject(msg);
-		if (data_out.isError) assert(false, "KeAllocateMemRefObject failed!");
-		error e = KePushMessage(chan, data_out);
-		if (e) printf("error pushing message: {}", cast(long)e);
+		assert_success(data_out);
+		assert_success(KePushMessage(chan, data_out));
 		printf("sent message: {}", msg);
 	}
 
 	{
 		XHandle msg = KePopMessage(chan);
-		if (msg.isError) printf("error popping message: {}", msg.getError);
+		assert_success(msg);
 		if (msg.getType() != type.memref)
 			assertf(false, "popped data handle is of type {}, expected type::memref", msg.getType());
 		
 		ulong msg_size = KeGetMemObjectSize(msg);
 		char* data_in = cast(char*)malloc(msg_size + 1);
-		KeReadMemory(msg, 0, msg_size, data_in);
+		assert_success(KeReadMemory(msg, 0, msg_size, data_in));
 		printf("recieved message: {}", data_in);
 	}
 
