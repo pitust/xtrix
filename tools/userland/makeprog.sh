ac6b2ed923ed997ab1599521846b1d2c15d5fba1
pitust <piotr@stelmaszek.com>
2021-11-13 13:12:16 +0000

    tools/userland: link with libxk

--- tools/userland/makeprog.sh
+++ tools/userland/makeprog.sh
@@ -6,4 +6,4 @@ ldc2 -mattr=-sse,-sse2,-sse3,-ssse3 -code-model=small -I src \
     --relocation-model=static --gdwarf --oq \
     --od build/user/$1 `find src/progs/$1 -type f | grep '\.d'` \
     -c --threads $(nproc) --betterC -mtriple=x86_64-elf
-ld.lld build/libxtrix.o build/user/$1/*.o -o build/$1.elf -T src/userland.ld
+ld.lld build/libxtrix.o build/libxk.o build/user/$1/*.o -o build/$1.elf -T src/userland.ld
