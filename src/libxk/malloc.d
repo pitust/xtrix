a7aef0175617bab2100bbb52575ed983998f8057
pitust <piotr@stelmaszek.com>
2021-11-16 21:21:14 +0000

    libxk/malloc: create alloc and free wrappers like in the kernel

--- src/libxk/malloc.d
+++ src/libxk/malloc.d
@@ -2,3 +2,16 @@ module libxk.malloc;
 
 extern(C) void libxk_sized_free(ulong size, void* pointer);
 extern(C) void* libxk_sized_malloc(ulong size);
+
+T* alloc(T, Args...)(Args args) {
+    import core.lifetime;
+    T* val = cast(T*)libxk_sized_malloc(T.sizeof);
+    emplace(val, args);
+    return val;
+}
+
+void free(T)(T* data) {
+    destroy!(false)(data);
+    libxk_sized_free(T.sizeof, cast(void*)data);
+}
+alias release = free;
