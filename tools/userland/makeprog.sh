5544812083e427a60336b0472aafdc313d37a443
pitust <piotr@stelmaszek.com>
2021-11-17 10:05:56 +0000

    tools/makeprog: allow for library linking

--- tools/userland/makeprog.sh
+++ tools/userland/makeprog.sh
@@ -6,4 +6,6 @@ ldc2 -mattr=-sse,-sse2,-sse3,-ssse3 -code-model=small -I src \
     --relocation-model=static --gdwarf --oq \
     --od build/user/$1 `find src/progs/$1 -type f | grep '\.d'` \
     -c --threads $(nproc) --betterC -mtriple=x86_64-elf --flto=full
-ld.lld build/libxtrix.o build/libxk.o build/user/$1/*.o -o build/$1.elf -T src/userland.ld
+A0="$1"
+shift
+ld.lld $(printf "build/lib%s.o\n" xtrix xk $@) build/user/$A0/*.o -o build/$A0.elf -T src/userland.ld
