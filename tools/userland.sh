b33eb6bda3bfce8b750af479a255747b6ddbe0fe
pitust <piotr@stelmaszek.com>
2021-11-17 10:14:04 +0000

    tools/userland: init should be built with -lsrpc

--- tools/userland.sh
+++ tools/userland.sh
@@ -3,5 +3,5 @@ set -ex
 sh tools/userland/makelib.sh libxtrix
 sh tools/userland/makelib.sh libxk
 sh tools/userland/makelib.sh libsrpc
-sh tools/userland/makeprog.sh init
+sh tools/userland/makeprog.sh init srpc
 sh tools/userland/makeprog.sh hello
