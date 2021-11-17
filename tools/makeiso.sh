395d529650c617f2189dba795caa29e6fdae1480
pitust <piotr@stelmaszek.com>
2021-11-17 10:05:44 +0000

    tools/makeiso: make makeiso work with dash

--- tools/makeiso.sh
+++ tools/makeiso.sh
@@ -5,7 +5,7 @@ set -ex
 rm -rf pack
 mkdir -p pack/boot
 
-cp build/{init,hello,kernel}.elf pack/
+cp build/*.elf pack/
 cp assets/console.sfn pack
 cp limine.cfg $HOME/limine/limine.sys $HOME/limine/limine-cd.bin $HOME/limine/limine-eltorito-efi.bin pack/boot/
 xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-eltorito-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o extreme.iso
