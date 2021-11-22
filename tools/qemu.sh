4801590ce1d84ee5b3d239a0f03c9565e3a9f6fd
pitust <piotr@stelmaszek.com>
2021-11-22 17:43:13 +0000

    tools: finalize rebranding (took me a while)

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -1,3 +1,3 @@
 set -ex
 
-sh tools/all.sh && reset && qemu-system-x86_64 -drive format=raw,file=extreme.iso -debugcon stdio -s
+sh tools/all.sh && reset && qemu-system-x86_64 -drive format=raw,file=xtrix.iso -debugcon stdio -s
