a6f12c6e346d0bb55d5c8bf7ae630ea4d0120837
pitust <piotr@stelmaszek.com>
2021-11-24 20:08:37 +0000

    launch qemu with the new stuff

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -1,3 +1,9 @@
 set -ex
 
-sh tools/all.sh && reset && qemu-system-x86_64 -drive format=raw,file=xtrix.iso -debugcon stdio -s
+sh tools/all.sh && reset
+
+if [ ! -e .qemu-debugcon-addr ]; then
+    echo "/dev/null" >.qemu-debugcon-addr
+fi
+clear >"$(cat .qemu-debugcon-addr)"
+qemu-system-x86_64 -drive format=raw,file=xtrix.iso -debugcon "file:$(cat .qemu-debugcon-addr)" -s
