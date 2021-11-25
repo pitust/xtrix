7c801706cadaa79574b840788eb277f24dbb748b
pitust <piotr@stelmaszek.com>
2021-11-25 18:11:22 +0000

    add a second clear just in case

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -6,5 +6,6 @@ if [ ! -e .qemu-debugcon-addr ]; then
     echo "/dev/null" >.qemu-debugcon-addr
 fi
 printf "\e[3g\ec\e[!p\e[?3;4l\e[4l\e>" >"$(cat .qemu-debugcon-addr)"
+clear >"$(cat .qemu-debugcon-addr)"
 qemu-system-x86_64 -drive format=raw,file=xtrix.iso -debugcon "file:$(cat .qemu-debugcon-addr)" -serial stdio -s -no-reboot
 printf "\n\n[system reset]\n\n" >"$(cat .qemu-debugcon-addr)"
