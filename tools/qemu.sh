9936f2ec6b11ac8b7d1cd2ff12569dd6b06b6369
pitust <piotr@stelmaszek.com>
2021-11-25 18:11:38 +0000

    add only  one newline when qemu decides to die

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -8,4 +8,4 @@ fi
 printf "\e[3g\ec\e[!p\e[?3;4l\e[4l\e>" >"$(cat .qemu-debugcon-addr)"
 clear >"$(cat .qemu-debugcon-addr)"
 qemu-system-x86_64 -drive format=raw,file=xtrix.iso -debugcon "file:$(cat .qemu-debugcon-addr)" -serial stdio -s -no-reboot
-printf "\n\n[system reset]\n\n" >"$(cat .qemu-debugcon-addr)"
+printf "\n[system reset]\n\n" >"$(cat .qemu-debugcon-addr)"
