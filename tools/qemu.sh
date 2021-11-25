0ee757bf1096dc8ab3380d20d99d7f6010b500d6
pitust <piotr@stelmaszek.com>
2021-11-25 18:11:12 +0000

    change the clear escapes to make vscode like us

--- tools/qemu.sh
+++ tools/qemu.sh
@@ -5,6 +5,6 @@ sh tools/all.sh && reset || exit
 if [ ! -e .qemu-debugcon-addr ]; then
     echo "/dev/null" >.qemu-debugcon-addr
 fi
-clear >"$(cat .qemu-debugcon-addr)"
+printf "\e[3g\ec\e[!p\e[?3;4l\e[4l\e>" >"$(cat .qemu-debugcon-addr)"
 qemu-system-x86_64 -drive format=raw,file=xtrix.iso -debugcon "file:$(cat .qemu-debugcon-addr)" -serial stdio -s -no-reboot
 printf "\n\n[system reset]\n\n" >"$(cat .qemu-debugcon-addr)"
