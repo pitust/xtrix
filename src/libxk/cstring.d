98c721bbbe19b4b80888b7a6b47fbd67ef6503b7
pitust <piotr@stelmaszek.com>
2021-12-17 07:01:37 +0000

    add copyright note

--- src/libxk/cstring.d
+++ src/libxk/cstring.d
@@ -1,3 +1,18 @@
+// libxk mem{cpy,set,cmp} declarations
+// Copyright (C) 2021 pitust <piotr@stelmaszek.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 module libxk.cstring;
 
 extern (C) byte* memcpy(byte* dst, const byte* src, size_t n);
